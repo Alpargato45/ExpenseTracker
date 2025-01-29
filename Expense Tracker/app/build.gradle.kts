a578521a0cd201160ecafab528f2f5cf7d69e285
Jorge del Cid <jorgedelcid2004@gmail.com>
2025-01-29 14:13:45 +0100

    Añadir Fragments

--- Expense Tracker/app/build.gradle.kts
+++ Expense Tracker/app/build.gradle.kts
@@ -36,4 +36,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+
 }
--- Expense Tracker/app/src/main/java/com/alpargato/expensetracker/MainActivity.java
+++ Expense Tracker/app/src/main/java/com/alpargato/expensetracker/MainActivity.java
@@ -3,12 +3,51 @@ package com.alpargato.expensetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.alpargato.expensetracker.Fragments.HomeFragment;
+import com.alpargato.expensetracker.Fragments.NotificationFragment;
+import com.alpargato.expensetracker.Fragments.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    private HomeFragment homeFragment = new HomeFragment();
+    private NotificationFragment notificationFragment = new NotificationFragment();
+    private SettingsFragment settingsFragment = new SettingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        //BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.notification);
+        //badgeDrawable.setVisible(true);
+        //badgeDrawable.setNumber(8);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int itemIndex = item.getItemId();
+                if (itemIndex == R.id.home) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                    return true;
+                }else if (itemIndex == R.id.notification) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,notificationFragment).commit();
+                    return true;
+                }else if (itemIndex == R.id.settings) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,settingsFragment).commit();
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        });
     }
 }
